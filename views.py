5cac857022c7aefa677343619b04d425500ec412
rogeriovc <rogervclaporta@gmail.com>
2024-08-06 19:18:45 -0400

    Delete views.py

--- views.py
+++
@@ -1,6 +0,0 @@
-def menu_inicial():
-    print("1-criar um cachorro\n2-criar um humano\n3-listar caos\n4-listar humanos\n5-sair\n-------------")
-
-def sub_menu():
-    print("1-andando\n2-latir\n3-info.do cao\n4-info.humano\n5-voltar ao principal\n6-sair\n-------------")
-
